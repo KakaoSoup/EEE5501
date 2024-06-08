be43d199f6be0ee540d852da4e35ff60132bb680
KakaoSoup <kspaul47@gmail.com>
2024-06-08 20:48:12 +0900

    write comment

--- HW4/mpi/dft.h
+++ HW4/mpi/dft.h
@@ -15,28 +15,33 @@ template <typename T>
 void dft1d(T* data, int n, T* weights) {
     if (n <= 1) return;
 
-    // Divide
+    // Divide in evne and odd
     T* even = new T[n / 2];
     T* odd = new T[n / 2];
 
+    // reorder it with even number first and odd number later.
     int i = 0, j = 0, k = 0;
     for (i = 0; i < n / 2; i++) {
         even[i] = data[i * 2];
         odd[i] = data[i * 2 + 1];
     }
 
+    // reorder 'data' with even -> odd
     i = 0; j = 0; k = 0;
     while(k < n / 2)
 	    data[k++] = even[i++];
     while(k < n)
 	    data[k++] = odd[j++];
 
+    // delete the copies
     delete[] even;
     delete[] odd;
 
+    // recursive call of dft1d
     dft1d(data, n/2, weights);
     dft1d(&data[n/2], n/2, weights);
 
+    // temp array to calculate
     T* temp = new T[n];
 
     // Combine
@@ -46,26 +51,16 @@ void dft1d(T* data, int n, T* weights) {
         temp[k + n / 2] = data[k] - W;
     }
 
+    // copy temp to data
     for(i = 0; i < n; i++)
 	    data[i] = temp[i];
-
     delete[] temp;
-}
-
-// Function to transpose a square matrix
-template <typename T>
-void transpose(T* data, unsigned width, unsigned height) {
-    for (unsigned i = 0; i < height; ++i) {
-        for (unsigned j = i + 1; j < width; ++j) {
-            std::swap(data[i * width + j], data[j * width + i]);
-        }
-    }
 }
 
 // Perform 2-D discrete Fourier transform (DFT)
 template <typename T>
 void dft2d(T* data, const unsigned width, const unsigned height, const int num_ranks, const int rank_id) {
-    // Precalculate weights for the FFT
+    // Precalculate weights for the FT
     T* weights = new T[width / 2];
     for (unsigned k = 0; k < width / 2; k++) {
         weights[k] = std::polar(1.0, -2 * M_PI * k / width);
@@ -73,20 +68,23 @@ void dft2d(T* data, const unsigned width, const unsigned height, const int num_r
 
     // a. Row-wise one-dimensional DFTs
     for (unsigned i = 0; i < height; i++) {
+	// run dft1d for rows which are multiples of rank_id
         if ((i % num_ranks) == (unsigned)rank_id) {
             dft1d(&data[width * i], width, weights);
         }
     }
 
-    // Allocate buffer for received data
+    // buffer for received data
     T* recv_buffer = new T[width * height];
+    // MPI requests to check other processor's data transfer
     MPI_Request* requests = new MPI_Request[num_ranks * height];
+    // count reqeusts
     int request_count = 0;
 
+    // receive data from other processes to fill uncalculated values
     for (int i = 0; i < num_ranks; i++) {
         if (i != rank_id) {
             for (unsigned j = i; j < height; j += num_ranks) {
-		// recevie data from other processes
                 MPI_Irecv(&recv_buffer[width * j], width, MPI_DOUBLE_COMPLEX, i, j, MPI_COMM_WORLD, &requests[request_count++]);
             }
         }
@@ -96,6 +94,7 @@ void dft2d(T* data, const unsigned width, const unsigned height, const int num_r
     for (int i = 0; i < num_ranks; i++) {
         if (i != rank_id) {
             for (unsigned j = rank_id; j < height; j += num_ranks) {
+		// send the datas which are calculated in this processor
                 MPI_Send(&data[width * j], width, MPI_DOUBLE_COMPLEX, i, j, MPI_COMM_WORLD);
             }
         }
@@ -129,7 +128,7 @@ void dft2d(T* data, const unsigned width, const unsigned height, const int num_r
     // Reset the request count
     request_count = 0;
 
-    // Post non-blocking receives for transposed data
+    // receive data from other processes to fill uncalculated values
     for (int i = 0; i < num_ranks; i++) {
         if (i != rank_id) {
             for (unsigned j = i; j < width; j += num_ranks) {
@@ -142,11 +141,11 @@ void dft2d(T* data, const unsigned width, const unsigned height, const int num_r
     for (int i = 0; i < num_ranks; i++) {
         if (i != rank_id) {
             for (unsigned j = rank_id; j < width; j += num_ranks) {
+		// send the datas which are calculated in this processor
                 MPI_Send(&transposed[height * j], height, MPI_DOUBLE_COMPLEX, i, j, MPI_COMM_WORLD);
             }
         }
     }
-
 
     // Wait for all non-blocking receives to complete
     MPI_Waitall(request_count, requests, MPI_STATUSES_IGNORE);
